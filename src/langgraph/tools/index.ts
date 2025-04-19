45daad223a7d31c9fbd744b6e0c91b2339b4576d
Jonatan Bortolon <jonatangabrielbortolon@outlook.com>
2025-04-19 18:48:55 -0300

    Fix lint

--- src/langgraph/tools/index.ts
+++ src/langgraph/tools/index.ts
@@ -1,9 +1,9 @@
 import { createTransactionTool } from "@/langgraph/tools/create-transaction";
 import { deleteTransactionTool } from "@/langgraph/tools/delete-transaction";
 import { monthSpentAndReceivedTool } from "@/langgraph/tools/month-spent-and-received";
+import { lastMonthSpentAndReceivedTool } from "./last-month-spent-and-received";
 import { monthSpentByBankAccountTool } from "./month-spent-by-bank-account";
 import { monthSpentByCategoryTool } from "./month-spent-by-category";
-import { lastMonthSpentAndReceivedTool } from "./last-month-spent-and-received";
 
 export const tools = [
 	createTransactionTool,
